8fcd97f11b25cab962c5af6e795a602ffeef0486
zuekiin <elio.cadiz@gmail.com>
2025-05-30 00:34:36 +0200

    Ett kommaT

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ function showContacts(contactBook, groupToShow) {
   // Rör ej denna kod
   for (let contact of contactBook.contacts) {
     if (contact.group === groupToShow) {
-      return `Namn: ${contact.name} Telefon: ${contact.phone}`;
+      return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
     }
   }
 
